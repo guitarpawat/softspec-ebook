c918d922bf5a51c3c2bb0a0181c1b7e46a62529d
Pawat Nakpiphatkul <guitar.pawat@gmail.com>
2018-04-19 15:38:09 +0700

    Initialize phase 3

---
+++ app/src/main/java/pawat/guitar/ebook/AccountActivity.kt
@@ -0,0 +1,12 @@
+package pawat.guitar.ebook
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class AccountActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_account)
+    }
+}
